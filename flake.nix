b0091e33755a222ed70058fef580bbc72b87e291
Jermeiah S <owner@arouzing.xyz>
2024-09-15 18:09:21 -0400

    auto import new modules

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       self,
       clan-core,
       flake-parts,
+      nixpkgs,
       ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -22,52 +23,53 @@
       ];
       imports = [ inputs.clan-core.flakeModules.default ];
       # https://docs.clan.lol/getting-started/flake-parts/
-      clan = {
-        meta.name = "testclan"; # Ensure this is unique among all clans you want to use.
+      clan =
+        let
+          fs = nixpkgs.lib.fileset;
+          nixosModules = fs.toList ./modules;
+        in
+        {
+          meta.name = "testclan"; # Ensure this is unique among all clans you want to use.
 
-        # Make flake available in modules
-        specialArgs.self = {
-          inherit (self) inputs nixosModules packages;
-        };
-        directory = self;
-        machines = {
-          # "jon" will be the hostname of the machine
-          jon =
-            { pkgs, ... }:
-            {
-              imports = [
-                ./modules/shared.nix
-                ./modules/disko.nix
-                ./machines/jon/configuration.nix
-              ];
+          # Make flake available in modules
+          specialArgs.self = {
+            inherit (self) inputs nixosModules packages;
+          };
+          directory = self;
+          machines = {
+            # "jon" will be the hostname of the machine
+            jon =
+              { pkgs, ... }:
+              {
+                imports = nixosModules ++ [ ./machines/jon/configuration.nix ];
 
-              nixpkgs.hostPlatform = "x86_64-linux";
+                nixpkgs.hostPlatform = "x86_64-linux";
 
-              # Set this for clan commands use ssh i.e. `clan machines update`
-              # If you change the hostname, you need to update this line to root@<new-hostname>
-              # This only works however if you have avahi running on your admin machine else use IP
-              clan.core.networking.targetHost = pkgs.lib.mkDefault "root@jon";
+                # Set this for clan commands use ssh i.e. `clan machines update`
+                # If you change the hostname, you need to update this line to root@<new-hostname>
+                # This only works however if you have avahi running on your admin machine else use IP
+                clan.core.networking.targetHost = pkgs.lib.mkDefault "root@jon";
 
-              # You can get your disk id by running the following command on the installer:
-              # Replace <IP> with the IP of the installer printed on the screen or by running the `ip addr` command.
-              # ssh root@<IP> lsblk --output NAME,ID-LINK,FSTYPE,SIZE,MOUNTPOINT
-              disko.devices.disk.main = {
-                device = "/dev/disk/by-id/ata-QEMU_DVD-ROM_QM00003";
+                # You can get your disk id by running the following command on the installer:
+                # Replace <IP> with the IP of the installer printed on the screen or by running the `ip addr` command.
+                # ssh root@<IP> lsblk --output NAME,ID-LINK,FSTYPE,SIZE,MOUNTPOINT
+                disko.devices.disk.main = {
+                  device = "/dev/disk/by-id/ata-QEMU_DVD-ROM_QM00003";
+                };
+
+                # IMPORTANT! Add your SSH key here
+                # e.g. > cat ~/.ssh/id_ed25519.pub
+                users.users.root.openssh.authorizedKeys.keys = [
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAGm66rJsr8vjRCYDkH4lEPncPq27o6BHzpmRmkzOiM"
+                  "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBA9i9HoP7X8Ufzz8rAaP7Nl3UOMZxQHMrsnA5aEQfpTyIQ1qW68jJ4jGK5V6Wv27MMc3czDU1qfFWIbGEWurUHQ="
+                ];
+
+                # Zerotier needs one controller to accept new nodes. Once accepted
+                # the controller can be offline and routing still works.
+                clan.core.networking.zerotier.controller.enable = true;
               };
-
-              # IMPORTANT! Add your SSH key here
-              # e.g. > cat ~/.ssh/id_ed25519.pub
-              users.users.root.openssh.authorizedKeys.keys = [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAGm66rJsr8vjRCYDkH4lEPncPq27o6BHzpmRmkzOiM"
-                "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBA9i9HoP7X8Ufzz8rAaP7Nl3UOMZxQHMrsnA5aEQfpTyIQ1qW68jJ4jGK5V6Wv27MMc3czDU1qfFWIbGEWurUHQ="
-              ];
-
-              # Zerotier needs one controller to accept new nodes. Once accepted
-              # the controller can be offline and routing still works.
-              clan.core.networking.zerotier.controller.enable = true;
-            };
+          };
         };
-      };
       perSystem =
         { pkgs, inputs', ... }:
         {
